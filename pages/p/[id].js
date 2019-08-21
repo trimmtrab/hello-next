932bf73fba3276b9d574b74c9f1fdea7d9cc0aae
Artem Zimin <atm.zmn95@gmail.com>
2019-08-21 11:57:43 +0300

    add clean urls at dynamic post pages

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,16 +1,16 @@
 import { useRouter } from 'next/router';
 
-import Layout from '../components/MyLayout';
+import Layout from '../../components/MyLayout';
 
 const Post = () => {
   const router = useRouter();
 
   return (
     <Layout>
-      <h1>{router.query.title}</h1>
+      <h1>{router.query.id}</h1>
       <p>This is the blog post content.</p>
     </Layout>
   );
-};
+}
 
 export default Post;
